e5fe7255bb220001b93dde3694983463eed751fb
jongha kim <jjhsun99@gmail.com>
2024-12-11 17:55:01 +0900

    helloworld

---
+++ webRtc-hello-world/src/main/java/com/example/webrtchelloworld/WebRtcHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.webrtchelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRtcHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebRtcHelloWorldApplication.class, args);
+    }
+
+}
---
+++ webRtc-hello-world/src/test/java/com/example/webrtchelloworld/WebRtcHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webrtchelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRtcHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

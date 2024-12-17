1b995cd84b37777502fb9d0cb66995b9f6a7c888
jongha kim <jjhsun99@gmail.com>
2024-12-17 16:04:37 +0900

    feat: webRtp start

---
+++ webRtc-rtp-receiver/src/main/java/com/example/webrtcrtpreceiver/WebRtcRtpReceiverApplication.java
@@ -0,0 +1,13 @@
+package com.example.webrtcrtpreceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRtcRtpReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRtcRtpReceiverApplication.class, args);
+	}
+
+}
---
+++ webRtc-rtp-receiver/src/test/java/com/example/webrtcrtpreceiver/WebRtcRtpReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webrtcrtpreceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRtcRtpReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

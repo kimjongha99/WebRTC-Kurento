5b130f933411a4cc89b221efb182cd29298dceb9
jongha kim <jjhsun99@gmail.com>
2024-12-23 12:23:10 +0900

    group

---
+++ group-call/src/main/java/com/example/groupcall/GroupCallApplication.java
@@ -0,0 +1,58 @@
+package com.example.groupcall;
+
+import org.kurento.client.KurentoClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+
+@SpringBootApplication
+@EnableWebSocket
+
+public class GroupCallApplication implements WebSocketConfigurer {
+    @Bean
+    public CallHandler handler() {
+        return new CallHandler();
+    }
+
+    @Bean
+    public UserRegistry registry() {
+        return new UserRegistry();
+    }
+
+    @Bean
+    public RoomManager roomManager() {
+        return new RoomManager();
+    }
+
+    @Bean
+    public CallHandler groupCallHandler() {
+        return new CallHandler();
+    }
+
+    @Bean
+    public KurentoClient kurentoClient() {
+        return KurentoClient.create();
+    }
+    @Bean
+    public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(32768);
+        return container;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(handler(), "/webrtc");
+    }
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupCallApplication.class, args);
+    }
+
+}
---
+++ group-call/src/test/java/com/example/groupcall/GroupCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groupcall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupCallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
